2e4cea9305cc58e73daa199524497eee7b7b4ea1
Michele Buccoli <m.buccoli@gmail.com>
2020-04-05 15:49:02 +0200

    dummy circuit; refactoring code

--- src/circuit/app.js
+++ src/circuit/app.js
@@ -1,28 +1,40 @@
- let BPM=0;
- let moving=0;
+ let BPM=2000;
+ 
  let angle=0;
  let camera=null;
  let scene=null;
- let ring=null;
  let RADIUS=48;
 
  let MINSTEP=0.001;
- let DELTAHEAD=0.05
  let HALF_WIDTH=2;
  let stepsize=1;
  let lastT=null;
  let curT=0;
  let lambdaBPM=0.8;
  let inertiaBPM=0.995;
-
+ let always_steady=true;
+ let path=0;
+ let PATH_1_TH=28;
+ let PATH_2_TH=32;
+ let PATH_3_TH=50;
+ let PATH_4_TH=60;
+ let PATH_5_TH=67;
+ let PATH_6_TH=81;
+ let PATH_7_TH=93;
+ let PATH_8_TH=118;
+ let PATH_9_TH=169;
+ let PATH_10_TH=178;
+ let PATH_11_TH=190;
  function getSeconds(){
 	let d = new Date();
 	return d.getMilliseconds()/1000.;
 }
 
  export  function updateBPM(newBPM){
+  if(always_steady){return;}
 	BPM = lambdaBPM*BPM + (1-lambdaBPM)*newBPM;
 	BPM= Math.max(0, BPM);
+  
 	return BPM;
 }
 
@@ -32,46 +44,75 @@
 };
 
 
-
-function createTrees(){
-  let tree=document.querySelector(".tree");
-  let forest=document.querySelector("#forest");
-  let num_trees=100;
-  let deltaRot=Math.round(360/(num_trees-1));
-  let rotation= tree.object3D.rotation.y;
-  let dist=7;
-  let sign=1;
-  for(let i=0; i<num_trees-1; i++){
-    let newTree = tree.cloneNode(true);
-    let position=RADIUS+sign*dist;
-    rotation=rotation+deltaRot;
-
-    newTree.setAttribute("rotation","0 "+ rotation+" 0");
-    newTree.children[0].setAttribute("position", position +" 0 0");
-    forest.appendChild(newTree);    
-    sign=sign*-1;
-  }
-  tree.children[0].children[1].setAttribute("color","black");
- }
-
-
  function step(deltaStep){            
   if(camera===null){ 
     console.log("Problem");
   }
+  let angle=0;
+  path += deltaStep;
   if(deltaStep==0){return;}
+  if(path < PATH_1_TH){    
+    console.log(path);
+  }
+  else if(path < PATH_2_TH){
+    angle = 0.25*Math.PI*(path-PATH_1_TH)/(PATH_2_TH-PATH_1_TH);
+    console.log("Angle is " + (angle*180/Math.PI));
+    
+  }
+  else if(path < PATH_3_TH){
+    angle = 0.25*Math.PI;
+  }
+  else if(path < PATH_4_TH){
+    angle = Math.PI*(0.25-0.5*(path-PATH_3_TH)/(PATH_4_TH-PATH_3_TH));    
+  }
 
+  else if(path < PATH_5_TH){    
+    angle = -0.25*Math.PI;
+  }
+  else if(path < PATH_6_TH){    
+   // BPM=200;
+    angle = Math.PI*(-0.25+0.25*(path-PATH_5_TH)/(PATH_6_TH-PATH_5_TH)); 
+  }
+  else if(path < PATH_7_TH){    
+    
+    angle = 0;//Math.PI*(-0.25+0.25*(path-PATH_5_TH)/(PATH_6_TH-PATH_5_TH)); 
+  }
+  else if(path < PATH_8_TH){    
+    
+   // BPM=200;
+    angle = Math.PI*0.5*(path-PATH_7_TH)/(PATH_8_TH-PATH_7_TH); 
+  }
+  else if(path < PATH_9_TH){    
+    angle = Math.PI*0.50;//-0.25*Math.PI;
+  }
+  else if(path < PATH_10_TH){    
+    BPM=200;
+    angle = Math.PI*(0.5+0.5*(path-PATH_9_TH)/(PATH_10_TH-PATH_9_TH)); 
+     
+   // BPM=200;
+    //angle = Math.PI*0.5*(path-PATH_7_TH)/(PATH_8_TH-PATH_7_TH); 
+  }    
+  else{
+    angle = Math.PI;//-0.25*Math.PI;
+    BPM=0;
+  }
+  console.log("Angle is " + (angle*180/Math.PI));
+  camera.position.z =camera.position.z-deltaStep*Math.cos(angle);
+  camera.position.x =camera.position.x+deltaStep*Math.sin(angle);
+  camera.rotation.y = -angle;//*180/Math.PI; 
+  /*
   angle = angle + deltaStep/RADIUS;
   camera.position.x=RADIUS*Math.cos(-angle);
   camera.position.z=RADIUS*Math.sin(-angle);
 
   //camera.position.y=camera.position.y+DELTAHEAD*Math.cos(2*Math.PI*curT*BPM/60.);
 
-  camera.rotation.y=angle;  
+  camera.rotation.y=angle;  */
 }   
 
 
 function update(){
+  console.log(camera.position);
   if(lastT===null){
     lastT=getSeconds();
     window.requestAnimationFrame(update);
@@ -80,8 +121,8 @@ function update(){
   curT=getSeconds();
   let deltaT=curT-lastT; //seconds
   //console.log(1/deltaT);
- let deltaStep= stepsize*BPM*deltaT/60.; // m
-  //console.log(deltaStep);
+  let deltaStep= stepsize*BPM*deltaT/60.; // m
+  console.log(deltaStep);
   if (deltaStep<MINSTEP){deltaStep=0;}
   step(deltaStep);
   
@@ -97,7 +138,7 @@ export  function load(){
   console.log("loading...");
   console.log(scene);
   if (!scene){
-    setInterval(()=>{
+    setTimeout(()=>{
         load();
     },5000);
     return "not loaded";
@@ -110,16 +151,7 @@ export  function load(){
 function sceneLoaded(){
   console.log("loaded?");    
   camera=window.AFRAME.scenes[0].camera;
-  //camera.position.x=RADIUS;
   camera.position.y=1.7; // 170 cm
-  /*ring= document.querySelector("#track");
-  ring.setAttribute("radius-inner",RADIUS-HALF_WIDTH);
-  ring.setAttribute("radius-outer",RADIUS+HALF_WIDTH);
-  createTrees();*/
-  //document.getElementById('entityModel').innerHTML='          <a-entity objModel="obj: #scene-obj; mtl: #scene-mtl"  shadow="cast: true" position="40 -1 20"></a-entity>  ';  
+  
   window.requestAnimationFrame(update);
 }
-document.addEventListener('keydown', function(event){
-        if(event.keyCode == 32) {
-          step();} }
-          );
